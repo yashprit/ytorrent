5bf05d1590dcf766cf2f3a91f625e4d0213ade1c
Yashprit Singh <yashprits@gmail.com>
2015-04-28 18:46:30 +0530

    added blessed layout

--- README.md
+++ README.md
@@ -2,39 +2,27 @@
 
 > yet another cli client for torrent writtern in node
 
-
 ## Install
 
 ```sh
-$ npm install --save ytorrent
+$ npm install --global ytorrent
 ```
-
 
 ## Usage
-
-### JavaScript
-
-```js
-var ytorrent = require('ytorrent');
-
-ytorrent('Rainbow');
-```
 
 ### Command Line reference
 
 ```sh
 $ npm install --global ytorrent
-$ ytorrent --help
+➜  ytorrent git:(master) ✗ ytorrent -s "magnet:?xt=urn:btih:4d753474429d817b80ff9e0c441ca660ec5d2450&dn=Ubuntu+14.04+64+bit&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337"
 ```
 
-## Run Test
-```sh
-npm test
-```
+*When you run above command you'll get below screen*
+
+![ScreenShot](https://raw.github.com/yashprit/ytorrent/master/screen_shot.png)
 
 ## Contribute or Report Issue
 For bugs and feature requests, [please create an issue][issue-url].
-
 
 ## License
 
--- index.js
+++ index.js
@@ -1,15 +1,56 @@
 'use strict';
 
 var torrentStream = require("torrent-stream");
+var blessed = require('blessed');
+var contrib = require('blessed-contrib');
 
-var torrentDl = function(src, opts) {
+exports.dl = function(src, opts) {
   var engine = torrentStream(src, opts);
   engine.on('ready', function() {
+
+    var screen = blessed.screen();
+
+    var grid = new contrib.grid({
+      rows: 12,
+      cols: 12,
+      screen: screen
+    });
+
     engine.files.forEach(function(file) {
       file.select();
     });
-  });
-  return engine;
-}
 
-module.exports = torrentDl;
+    var fileCount = engine.files.length;
+
+    var label = 'Downloading ' + fileCount + (fileCount === 1 ? ' file' : ' files');
+
+    var gauge = grid.set(0, 6, 6, 6, contrib.gauge, {
+      label: label
+    });
+
+    var log = grid.set(0, 0, 6, 6, contrib.log, {
+      fg: "green",
+      selectedFg: "green",
+      label: 'File Description'
+    });
+
+    var names = engine.files.map(function(f) {
+      return f.name.trim();
+    }).join('\n');
+
+    log.log(names);
+
+    var status = function() {
+      var percentage = ((engine.swarm.downloaded / engine.torrent.length) * 100).toPrecision(4)
+
+      if (percentage > 100) {
+        percentage = 100
+      }
+      gauge.setPercent(parseInt(percentage, 10));
+      screen.render();
+    }
+
+    var interval = setInterval(status, 100)
+    status();
+  });
+}
